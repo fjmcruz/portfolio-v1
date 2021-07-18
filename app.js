c8f2bbb84a51c4d5c9a415f86f99222412adc60e
Francis Cruz <francisjosephmcruz@gmail.com>
2021-07-18 23:58:29 +0800

    PRTFV1-0001: Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const navigationSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  // Toggle nav
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    // Animate links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navigationLinkFade 0.4s ease forwards ${
+          index / 6
+        }s`;
+      }
+    });
+    // Burger animation
+    burger.classList.toggle("toggle");
+  });
+};
+// Call navigation slide function
+navigationSlide();
+
+// Get current date for copyright footer
+const date = new Date();
+let year = date.getFullYear();
+document.getElementById("copyright").innerHTML += year;
